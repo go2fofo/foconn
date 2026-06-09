5baef8d995efbad2dc7d2d925d1e4b0f8658ac4e
fofo wang <313612895@qq.com>
2026-06-09 11:50:26 +0800

    first commit4

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,6 +1,14 @@
+/*
+ * @Author: fofo
+ * @Date: 2026-06-08 13:32:59
+ * @LastEditTime: 2026-06-09 11:50:01
+ * @LastEditors: fofo
+ * @Description: 
+ * @FilePath: /foconn/src-tauri/src/main.rs
+ */
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
 fn main() {
-    tauri_app_lib::run()
+    foconn_lib::run()
 }
